3aee3706402e00560ec9c1010f845c42838c71be
JUDEOTIM <otimjudeoj@gmail.com>
2023-03-29 05:21:29 -0700

    tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,86 +1,52 @@
 #!/usr/bin/python3
-""" Test User Unittest module
-"""
+"""Unittest module for the State Class."""
 
-from datetime import datetime, date
 import unittest
-from models.user import User
-from models.base_model import BaseModel
+from datetime import datetime
+import time
 from models.state import State
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
 
 
-class TestState_Object_instance(unittest.TestCase):
+class TestState(unittest.TestCase):
 
-    """ Test User class instance object
-    """
-
-    def setUp(self):
-        print("Testing State Object Instance")
-
-    def test_State_is_a_sublass_basemodel(self):
-        s1 = State()
-        self.assertIsInstance(s1, BaseModel)
-
-    def test_state_unique_ids(self):
-        s1 = State()
-        s2 = State()
-        self.assertNotEqual(s1.id, s2.id)
-
-    def test_state_name_default_value(self):
-        s1 = State()
-        self.assertEqual(s1.name, "")
-
-    def test_state_id_type(self):
-        s1 = State()
-        self.assertEqual(type(s1.id), str)
-
-    def test_state_object_creation_time(self):
-        s1 = State()
-        s2 = State()
-        self.assertNotEqual(s1.created_at, s2.created_at)
-
-    def test_state_name_attr(self):
-        s1 = State()
-        s1.name = "Lagos"
-        self.assertEqual(s1.name, "Lagos")
-
-    def test_state_name(self):
-        s1 = State()
-        self.assertEqual(s1.name, "")
-
-
-class TestState_Has_attr_basemodel(unittest.TestCase):
-
-    """ Test User for super class attributes
-    """
+    """Test Cases for the State class."""
 
     def setUp(self):
-        print("Testing if State has the attributes of BaseModel")
+        """Sets up test methods."""
+        pass
 
-    def test_state_has_id_attr(self):
-        s1 = State()
-        self.assertTrue(s1.id)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
 
-    def test_state_has_created_at_attr(self):
-        s1 = State()
-        self.assertTrue(s1.created_at)
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-    def test_state_has_updated_at_attr(self):
-        s1 = State()
-        self.assertTrue(s1.updated_at)
+    def test_8_instantiation(self):
+        """Tests instantiation of State class."""
 
-    def test_state_can_take_kwargs(self):
-        s2 = State(name="Al-Areef")
-        self.assertEqual(s2.name, "Al-Areef")
+        b = State()
+        self.assertEqual(str(type(b)), "<class 'models.state.State'>")
+        self.assertIsInstance(b, State)
+        self.assertTrue(issubclass(type(b), BaseModel))
 
-    def test_state_can_take_multiple_kwargs(self):
-        s2 = State(name="Al-Areef", partner="Ayobami")
-        self.assertEqual(s2.partner, "Ayobami")
-
-    def test_state_object_str_representation(self):
-        s1 = State()
-        self.assertIn("[State]", s1.__str__())
-
+    def test_8_attributes(self):
+        """Tests the attributes of State class."""
+        attributes = storage.attributes()["State"]
+        o = State()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,108 +1,52 @@
 #!/usr/bin/python3
-""" Test User Unittest module
-"""
+"""Unittest module for the User Class."""
 
-from datetime import datetime, date
 import unittest
+from datetime import datetime
+import time
 from models.user import User
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
 from models.base_model import BaseModel
 
 
-class TestUser_Object_instance(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
-    """ Test User class instance object
-    """
-
-    def setUp(self):
-        print("Testing User Object Instance")
-
-    def test_user_is_a_sublass_basemodel(self):
-        u1 = User()
-        self.assertIsInstance(u1, BaseModel)
-
-    def test_user_unique_ids(self):
-        u1 = User()
-        u2 = User()
-        self.assertNotEqual(u1.id, u2.id)
-
-    def test_user_email_default_value(self):
-        u1 = User()
-        self.assertEqual(u1.email, "")
-
-    def test_user_firstname_default_value(self):
-        u1 = User()
-        self.assertEqual(u1.first_name, "")
-
-    def test_user_password_default_value(self):
-        u1 = User()
-        self.assertEqual(u1.password, "")
-
-    def test_user_lastname_default_value(self):
-        u1 = User()
-        self.assertEqual(u1.last_name, "")
-
-    def test_user_id_type(self):
-        u1 = User()
-        self.assertEqual(type(u1.id), str)
-
-    def test_user_object_creation_time(self):
-        u1 = User()
-        u2 = User()
-        self.assertNotEqual(u1.created_at, u2.created_at)
-
-    def test_user_firstname_attr(self):
-        u1 = User()
-        u1.first_name = "Ayobami"
-        self.assertEqual(u1.first_name, "Ayobami")
-
-    def test_user_lastname_attr(self):
-        u1 = User()
-        u1.last_name = "Al-Areef"
-        self.assertEqual(u1.last_name, "Al-Areef")
-
-    def test_user_password_attr(self):
-        u1 = User()
-        u1.password = "Alx"
-        self.assertEqual(u1.password, "Alx")
-
-    def test_user_email_attr(self):
-        u1 = User()
-        u1.email = "Alx@example.com"
-        self.assertEqual(u1.email, "Alx@example.com")
-
-
-class TestUser_Has_attr_basemodel(unittest.TestCase):
-
-    """ Test User for super class attributes
-    """
+    """Test Cases for the User class."""
 
     def setUp(self):
-        print("Testing if User has the attributes of BaseModel")
+        """Sets up test methods."""
+        pass
 
-    def test_user_has_id_attr(self):
-        u1 = User()
-        self.assertTrue(u1.id)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
 
-    def test_user_has_created_at_attr(self):
-        u1 = User()
-        self.assertTrue(u1.created_at)
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-    def test_user_has_updated_at_attr(self):
-        u1 = User()
-        self.assertTrue(u1.updated_at)
+    def test_8_instantiation(self):
+        """Tests instantiation of User class."""
 
-    def test_user_can_take_kwargs(self):
-        u2 = User(name="Al-Areef")
-        self.assertEqual(u2.name, "Al-Areef")
+        b = User()
+        self.assertEqual(str(type(b)), "<class 'models.user.User'>")
+        self.assertIsInstance(b, User)
+        self.assertTrue(issubclass(type(b), BaseModel))
 
-    def test_user_can_take_multiple_kwargs(self):
-        u2 = User(name="Al-Areef", partner="Ayobami")
-        self.assertEqual(u2.partner, "Ayobami")
-
-    def test_user_object_str_representation(self):
-        u1 = User()
-        self.assertIn("[User]", u1.__str__())
-
+    def test_8_attributes(self):
+        """Tests the attributes of User class."""
+        attributes = storage.attributes()["User"]
+        o = User()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 if __name__ == "__main__":
     unittest.main()
