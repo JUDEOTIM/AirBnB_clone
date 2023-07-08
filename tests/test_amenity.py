3596707acd6ac7b301698a60c83ea250ec9bb073
JUDEOTIM <otimjude@gmail.com>
2023-07-08 00:26:50 -0700

    tests

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -47,5 +47,6 @@ class TestAmenity(unittest.TestCase):
         for k, v in attributes.items():
             self.assertTrue(hasattr(o, k))
             self.assertEqual(type(getattr(o, k, None)), v)
-        if __name__ == "__main__":
-          unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
